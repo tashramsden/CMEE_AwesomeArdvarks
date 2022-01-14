089e44cc250b393822c7a1819d43b8da3e345e14
JiwenSun <92090981+JiwenSun@users.noreply.github.com>
2022-01-14 18:15:11 +0000

    Added TAutoCorr.R -by Doris

---
+++ week2/code/TAutoCorr.R
@@ -0,0 +1,19 @@
+#rm(list=ls())
+load("../data/KeyWestAnnualMeanTemperature.RData")
+yeartempall<-ats$Temp
+yeartemp_n<-ats$Temp[1:99]
+yeartemp_n1<-ats$Temp[2:100]
+yearpair<-data.frame(yeartemp_n,yeartemp_n1)
+cor<-cor(yearpair$yeartemp_n,yearpair$yeartemp_n1)
+corlist<-rep(NA,9999)
+for(i in 1:10000){
+  shuffle<-sample(yeartempall,99,replace=FALSE)
+  corlist[i]<-cor(yeartemp_n,shuffle)
+}
+count<-0
+for(i in 1:length(corlist)){
+  if(corlist[i]>cor){
+    count<-count+1
+  }
+}
+fraction<-count/length(corlist)
