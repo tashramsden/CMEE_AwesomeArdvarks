b546c088b45d8862f7b91ed22836c5ba4d1740f1
JiwenSun <92090981+JiwenSun@users.noreply.github.com>
2022-01-14 19:36:23 +0000

    Update TAutoCorr.R

--- week2/code/TAutoCorr.R
+++ week2/code/TAutoCorr.R
@@ -1,12 +1,12 @@
-#rm(list=ls())
+rm(list=ls())
 load("../data/KeyWestAnnualMeanTemperature.RData")
 yeartempall<-ats$Temp
 yeartemp_n<-ats$Temp[1:99]
 yeartemp_n1<-ats$Temp[2:100]
 yearpair<-data.frame(yeartemp_n,yeartemp_n1)
 cor<-cor(yearpair$yeartemp_n,yearpair$yeartemp_n1)
-corlist<-rep(NA,9999)
-for(i in 1:10000){
+corlist<-rep(NA,99999)
+for(i in 1:100000){
   shuffle<-sample(yeartempall,99,replace=FALSE)
   corlist[i]<-cor(yeartemp_n,shuffle)
 }
