6554a5db01002dbae0b225906a660816f3d57655
atn21 <an.nguyen21@imperial.ac.uk>
2022-01-19 14:38:47 +0000

    Update

---
+++ week3/code/get_TreeHeight.R
@@ -0,0 +1,36 @@
+# Author: Awesome Aardvark
+# Script: get_TreeHeight.R
+# Desc: Takes a csv file name from the command line. 
+#       Calculates heights of trees given distance of each tree 
+#       from its base and angle to its top.
+# Arguments: 3 (args: csv for analysis,
+#               degrees:   The angle of elevation of tree,
+#               distance:  The distance from base of tree (e.g., meters))
+# Output: The heights of the tree, same units as "distance". Results saved to new csv file
+# Date: Dec 2021
+
+rm(list = ls())
+
+#read data from command line
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)==0) { 
+  stop("Require input file", call.=FALSE)
+} else if (length(args)==1) {
+  readfile <- read.csv(args)
+}
+
+#calculate tree heights
+TreeHeight <- function(degrees, distance){
+    radians <- degrees * pi / 180
+    height <- distance * tan(radians)
+    return (height)
+}
+
+#add Tree Height column to readfile 
+readfile$Tree.Height.m <- TreeHeight(readfile$Angle.degrees, readfile$Distance.m)
+
+#save file without extension
+name <- tools::file_path_sans_ext(basename(args))
+
+#save all data to csv file in results/
+write.csv(readfile, paste("../results/",name,"_treeheight.csv"), row.names=FALSE)
---
+++ week3/code/run_get_TreeHeight.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Author: Awesome Aarvarks An 
+# Script: run_get_TreeHeight.sh
+# Desc: test get_TreeHeight.R and get_TreeHeight.py with trees.csv
+# Arguments: 1 .csv file)
+# Date: Dec 2021
+
+echo -e "Testing get_TreeHeight.R \n"
+Rscript get_TreeHeight.R ../data/trees.csv
+
+echo -e "Testing get_TreeHeight.py"
+python3 get_TreeHeight.py ../data/trees.csv
